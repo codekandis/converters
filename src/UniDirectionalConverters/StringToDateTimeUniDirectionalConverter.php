b9f6ce6257caca65a3a9e8b30d51d2fea4a09b17
Christian Ramelow <info@codekandis.net>
2024-02-18 19:33:04 +0100

    Adds value exceptions to any `DateTime` converter.

--- src/UniDirectionalConverters/StringToDateTimeUniDirectionalConverter.php
+++ src/UniDirectionalConverters/StringToDateTimeUniDirectionalConverter.php
@@ -3,9 +3,11 @@ namespace CodeKandis\Converters\UniDirectionalConverters;
 
 use CodeKandis\Converters\AbstractDateTimeRelatedConverter;
 use CodeKandis\Converters\ExpectedTypes;
+use CodeKandis\Converters\ValidValues;
 use DateTime;
 use Override;
 use function is_string;
+use function sprintf;
 
 /**
  * Represents a uni-directional converter converting a string into a DateTime.
@@ -25,6 +27,16 @@ class StringToDateTimeUniDirectionalConverter extends AbstractDateTimeRelatedCon
 			throw $this->getInvalidTypeException( $value, ExpectedTypes::STRING );
 		}
 
-		return DateTime::createFromFormat( $this->format, $value, $this->timeZone );
+		$convertedValue = DateTime::createFromFormat( $this->format, $value, $this->timeZone );
+
+		if ( false === $convertedValue )
+		{
+			throw $this->getInvalidValueException(
+				$value,
+				sprintf( ValidValues::TEMPLATE_DATETIME_STRING_TEMPLATE, $this->format )
+			);
+		}
+
+		return $convertedValue;
 	}
 }
