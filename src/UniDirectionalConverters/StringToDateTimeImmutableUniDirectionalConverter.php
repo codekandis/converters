7abfab404bbb7d342e80eb493cdf92a18875665b
Christian Ramelow <info@codekandis.net>
2024-03-17 18:35:18 +0100

    Adds `DateTime` related converter interfaces and base class.

--- src/UniDirectionalConverters/StringToDateTimeImmutableUniDirectionalConverter.php
+++ src/UniDirectionalConverters/StringToDateTimeImmutableUniDirectionalConverter.php
@@ -1,12 +1,11 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\Converters\UniDirectionalConverters;
 
-use CodeKandis\Converters\AbstractConverter;
+use CodeKandis\Converters\AbstractDateTimeRelatedConverter;
 use CodeKandis\Converters\InvalidTypeExceptionInterface;
-use CodeKandis\Converters\UniDirectionalConverterInterface;
+use CodeKandis\Converters\UniDirectionalDateTimeRelatedConverterInterface;
 use CodeKandis\Converters\ValidTypes;
 use DateTimeImmutable;
-use DateTimeZone;
 use Override;
 use function is_string;
 
@@ -15,20 +14,8 @@ use function is_string;
  * @package codekandis/converters
  * @author Christian Ramelow <info@codekandis.net>
  */
-class StringToDateTimeImmutableUniDirectionalConverter extends AbstractConverter implements UniDirectionalConverterInterface
+class StringToDateTimeImmutableUniDirectionalConverter extends AbstractDateTimeRelatedConverter implements UniDirectionalDateTimeRelatedConverterInterface
 {
-	/**
-	 * Constructor method.
-	 * @param string $format The format of the timestamp string.
-	 * @param ?DateTimeZone $timeZone The time zone of the timestamp.
-	 */
-	public function __construct(
-		private readonly string        $format,
-		private readonly ?DateTimeZone $timeZone = null
-	)
-	{
-	}
-
 	/**
 	 * Converts from a string into a DateTimeImmutable value.
 	 * @param string $value The string value which has to be converted.
--- src/UniDirectionalConverters/StringToDateTimeUniDirectionalConverter.php
+++ src/UniDirectionalConverters/StringToDateTimeUniDirectionalConverter.php
@@ -1,12 +1,11 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\Converters\UniDirectionalConverters;
 
-use CodeKandis\Converters\AbstractConverter;
+use CodeKandis\Converters\AbstractDateTimeRelatedConverter;
 use CodeKandis\Converters\InvalidTypeExceptionInterface;
-use CodeKandis\Converters\UniDirectionalConverterInterface;
+use CodeKandis\Converters\UniDirectionalDateTimeRelatedConverterInterface;
 use CodeKandis\Converters\ValidTypes;
 use DateTime;
-use DateTimeZone;
 use Override;
 use function is_string;
 
@@ -15,20 +14,8 @@ use function is_string;
  * @package codekandis/converters
  * @author Christian Ramelow <info@codekandis.net>
  */
-class StringToDateTimeUniDirectionalConverter extends AbstractConverter implements UniDirectionalConverterInterface
+class StringToDateTimeUniDirectionalConverter extends AbstractDateTimeRelatedConverter implements UniDirectionalDateTimeRelatedConverterInterface
 {
-	/**
-	 * Constructor method.
-	 * @param string $format The format of the timestamp string.
-	 * @param ?DateTimeZone $timeZone The time zone of the timestamp.
-	 */
-	public function __construct(
-		private readonly string        $format,
-		private readonly ?DateTimeZone $timeZone = null
-	)
-	{
-	}
-
 	/**
 	 * Converts from a string into a DateTime value.
 	 * @param string $value The string value which has to be converted.
